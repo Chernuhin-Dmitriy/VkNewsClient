21d2ff219eb8bad9dfce3ea47d2a0ae3704b05db
Chernuhin-Dmitriy <chernuhin2020@mail.ru>
2025-06-17 13:58:53 +0300

    UDF и создание стэйта

--- app/src/main/java/com/example/vknewsclient/MainViewModel.kt
+++ app/src/main/java/com/example/vknewsclient/MainViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.vknewsclient.domain.FeedPost
 import com.example.vknewsclient.domain.StatisticItem
-import com.example.vknewsclient.ui.theme.NavigationItem
+import com.example.vknewsclient.ui.theme.HomeScreenState
 
 class MainViewModel : ViewModel() {
-    private val initialList = mutableListOf<FeedPost>().apply {
+    private val sourceList = mutableListOf<FeedPost>().apply {
         repeat(3) {
             add(
                 FeedPost(id = it)
@@ -16,8 +16,10 @@ class MainViewModel : ViewModel() {
         }
     }
 
-    private val _feedPosts = MutableLiveData<List<FeedPost>>(initialList)
-    val feedPosts: LiveData<List<FeedPost>> = _feedPosts
+    private val initialState = HomeScreenState.Posts(posts = sourceList)
+
+    private val _screenState = MutableLiveData<HomeScreenState>(initialState)
+    val screenState: LiveData<HomeScreenState> = _screenState
 
     fun updateCount(feedPost: FeedPost, item: StatisticItem) {
         val oldPosts = _feedPosts.value?.toMutableList() ?: mutableListOf()
